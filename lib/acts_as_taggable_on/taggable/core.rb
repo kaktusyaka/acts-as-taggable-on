4fcad9292bf3810ce050b6ece39bd57fe27685a4
Vitalya Shevtsov <vitaliy@cloudassess.com.au>
2016-03-24 15:21:17 +1000

    added organisation_id is null as extra condition

--- lib/acts_as_taggable_on/taggable/core.rb
+++ lib/acts_as_taggable_on/taggable/core.rb
@@ -178,7 +178,8 @@ module ActsAsTaggableOn::Taggable
             tagging_join = "JOIN #{ActsAsTaggableOn::Tagging.table_name} #{taggings_alias}" \
                 "  ON #{taggings_alias}.taggable_id = #{quote}#{table_name}#{quote}.#{primary_key}" +
                 " AND #{taggings_alias}.taggable_type = #{quote_value(base_class.name, nil)}" +
-                " AND #{taggings_alias}.tag_id = #{quote_value(tag.id, nil)}"
+                " AND #{taggings_alias}.tag_id = #{quote_value(tag.id, nil)}" +
+                " AND tags.organisation_id IS NULL"
 
             tagging_join << " AND " + sanitize_sql(["#{taggings_alias}.created_at >= ?", options.delete(:start_at)]) if options[:start_at]
             tagging_join << " AND " + sanitize_sql(["#{taggings_alias}.created_at <= ?", options.delete(:end_at)])   if options[:end_at]
@@ -225,7 +226,7 @@ module ActsAsTaggableOn::Taggable
         query = self
         query = self.select(select_clause.join(',')) unless select_clause.empty?
         query.joins(joins.join(' '))
-          .where("#{ conditions.join(' AND ')} AND tags.organisation_id IS NULL")
+          .where(conditions.join(' AND '))
           .group(group)
           .having(having)
           .order(order_by.join(', '))
